700306286290e00726c0007a0cb5804c23163e90
ROTNdev <rotn.edm@gmail.com>
2015-01-05 00:43:32 +1100

    Because reasons

--- src/main/java/me/rotn/java/skypenet/Keyboard.java
+++ src/main/java/me/rotn/java/skypenet/Keyboard.java
@@ -1,11 +1,14 @@
 package me.rotn.java.skypenet;
 
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 
 import static java.awt.event.KeyEvent.*;
 
-public class Keyboard {
+public class Keyboard implements ClipboardOwner {
 
     private Robot robot;
 
@@ -19,12 +22,17 @@ public class Keyboard {
 
     public void type(String message) {
         StringSelection selection = new StringSelection(message);
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, this);
         robot.keyPress(VK_CONTROL);
         robot.keyPress(VK_V);
         robot.keyRelease(VK_V);
         robot.keyRelease(VK_CONTROL);
         robot.keyPress(VK_ENTER);
         robot.keyRelease(VK_ENTER);
+    }
+
+    @Override
+    public void lostOwnership(Clipboard clipboard, Transferable contents) {
+        //cry
     }
 }
