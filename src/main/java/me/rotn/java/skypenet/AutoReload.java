1f62765dacd3d0bcc1302a3c3bbfe39f0be4de2c
ROTNdev <rotn.edm@gmail.com>
2015-01-05 13:59:33 +1100

    Debug!

--- src/main/java/me/rotn/java/skypenet/AutoReload.java
+++ src/main/java/me/rotn/java/skypenet/AutoReload.java
@@ -1,9 +1,11 @@
 package me.rotn.java.skypenet;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class AutoReload extends Thread {
@@ -29,7 +31,8 @@ public class AutoReload extends Thread {
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(Files.readAllBytes(Paths.get(file)));
             return Arrays.toString(md.digest());
-        } catch (Exception ignored) {
+        } catch (IOException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
         return null;
     }
